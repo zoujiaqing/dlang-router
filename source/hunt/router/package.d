5ec6cdb060b3bfed69f0e39c0a5db679c6f298c7
zoujiaqing <zoujiaqing@gmail.com>
2017-04-12 12:51:28 +0800

    add git ignore files

--- source/hunt/router/package.d
+++ source/hunt/router/package.d
@@ -1,6 +1,7 @@
 ﻿module hunt.router;
 
 public import hunt.router.define;
+public import hunt.router.config;
 public import hunt.router.router;
 public import hunt.router.route;
 public import hunt.router.routegroup;
